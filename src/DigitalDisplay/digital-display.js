5a5c773d165099352a8c24ee55f626c056b6bc23
YuuKohaku <soul.of.bytes@gmail.com>
2016-03-30 17:13:40 +0600

    1

--- src/DigitalDisplay/digital-display.js
+++ src/DigitalDisplay/digital-display.js
@@ -25,31 +25,51 @@ class DigitalDisplay {
 				command_data = {},
 				command = 'display'
 			}) => {
-				this.emitter.addTask('agent', {
-						_action: 'active-agents',
-						agent_type: 'SystemEntity'
+				this.emitter.addTask('workstation', {
+						_action: 'get-workstations-cache',
+						device_type: 'digital-display'
 					})
 					.then((res) => {
-						_.map(res, (user_id) => {
-							let cmd = CommandFactory.getCommand(org_merged.digital_display_options.type, _.merge({
-								address: workstation.digital_display_address,
-								command
-							}, command_data, org_merged.digital_display_options));
+						res = res['digital-display'];
+						let keys = _(res)
+							.filter(v => (v.attached_to == org_merged.id && !_(v.maintains)
+								.keys()
+								.intersection(_.castArray(workstation))
+								.isEmpty()))
+							.map('id')
+							.value();
 
-							let data = {
-								command,
-								data: cmd.toString('hex')
-							};
-							let to_join = ['digital-display.command', org_addr, user_id];
-							console.log("DD EMITTING", user_id, _.join(to_join, "."), cmd);
-							this.emitter.emit('broadcast', {
-								event: _.join(to_join, "."),
-								data
+						return this.emitter.addTask('workstation', {
+								_action: 'by-id',
+								workstation: keys
+							})
+							.then((res) => {
+								return Promise.map(_.values(res), (dd) => {
+									let cmd = CommandFactory.getCommand(dd.display_type, _.merge({
+										address: dd.maintains[workstation],
+										command
+									}, command_data, dd));
+
+									let data = {
+										command,
+										data: cmd.toString('hex')
+									};
+									_(dd.occupied_by)
+										.castArray()
+										.compact()
+										.map((user_id) => {
+											let to_join = ['digital-display.command', org_addr, user_id];
+											this.emitter.emit('broadcast', {
+												event: _.join(to_join, "."),
+												data
+											});
+										})
+										.value();
+
+								});
 							});
-						});
 					});
 			});
-
 			return Promise.resolve(true);
 		}
 		//API
