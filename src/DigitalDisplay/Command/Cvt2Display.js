833d9205bc4f3ffd06b0451dd681d2989fa5d17b
YuuKohaku <soul.of.bytes@gmail.com>
2016-12-06 18:28:14 +0700

    addr big endian

--- src/DigitalDisplay/Command/Cvt2Display.js
+++ src/DigitalDisplay/Command/Cvt2Display.js
@@ -3,7 +3,10 @@
 let AkisDisplay = require("./AkisDisplay");
 
 class Cvt2Display extends AkisDisplay {
-
+	static setAddress(address, msg) {
+		let addr = msg.slice(1, 3);;
+		addr.writeUInt16BE(_.parseInt(address));
+	}
 }
 
 
